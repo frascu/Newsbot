d8a297c7f8a4592515d4852ea2c4f947d57ddd0f
frascu <frascu@users.noreply.github.com>
2017-01-09 19:19:00 +0100

    Update dump database

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.4.10
 -- Dumped by pg_dump version 9.4.10
--- Started on 2016-11-26 18:32:29
+-- Started on 2017-01-09 19:16:17
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -13,9 +13,8 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
-DROP DATABASE newsbotdb;
 --
--- TOC entry 1995 (class 1262 OID 16393)
+-- TOC entry 2007 (class 1262 OID 16393)
 -- Name: newsbotdb; Type: DATABASE; Schema: -; Owner: postgres
 --
 
@@ -42,7 +41,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1998 (class 0 OID 0)
+-- TOC entry 2010 (class 0 OID 0)
 -- Dependencies: 1
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -52,9 +51,41 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 SET search_path = public, pg_catalog;
 
+--
+-- TOC entry 175 (class 1259 OID 16458)
+-- Name: news_pk_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE news_pk_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE news_pk_seq OWNER TO postgres;
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
+
+--
+-- TOC entry 174 (class 1259 OID 16438)
+-- Name: news; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE news (
+    id bigint DEFAULT nextval('news_pk_seq'::regclass) NOT NULL,
+    link character varying(255),
+    title character varying(255),
+    pubblication_date date,
+    creation_date date,
+    sent character varying
+);
+
+
+ALTER TABLE news OWNER TO postgres;
 
 --
 -- TOC entry 173 (class 1259 OID 16413)
@@ -73,7 +104,25 @@ CREATE TABLE user_telegram (
 ALTER TABLE user_telegram OWNER TO postgres;
 
 --
--- TOC entry 1881 (class 2606 OID 16419)
+-- TOC entry 1891 (class 2606 OID 16449)
+-- Name: NEWS_PK; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY news
+    ADD CONSTRAINT "NEWS_PK" PRIMARY KEY (id);
+
+
+--
+-- TOC entry 1893 (class 2606 OID 16447)
+-- Name: link_unique; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+--
+
+ALTER TABLE ONLY news
+    ADD CONSTRAINT link_unique UNIQUE (link);
+
+
+--
+-- TOC entry 1889 (class 2606 OID 16419)
 -- Name: user_id_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -82,7 +131,7 @@ ALTER TABLE ONLY user_telegram
 
 
 --
--- TOC entry 1997 (class 0 OID 0)
+-- TOC entry 2009 (class 0 OID 0)
 -- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -93,7 +142,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2016-11-26 18:32:29
+-- Completed on 2017-01-09 19:16:17
 
 --
 -- PostgreSQL database dump complete

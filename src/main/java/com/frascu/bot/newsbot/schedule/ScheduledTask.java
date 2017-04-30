9e6aa4f08aad6fd9c6bef829f74e3d2ec3de629f
frascu <frascu@users.noreply.github.com>
2017-05-01 00:40:49 +0200

    Replace the Quartz Scheduler with the Timer in the java util

--- src/main/java/com/frascu/bot/newsbot/schedule/ScheduledTask.java
+++ src/main/java/com/frascu/bot/newsbot/schedule/ScheduledTask.java
@@ -3,45 +3,43 @@ package com.frascu.bot.newsbot.schedule;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.log4j.Logger;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import com.frascu.bot.newsbot.dao.NewsDao;
 import com.frascu.bot.newsbot.dto.NewsDto;
 import com.frascu.bot.newsbot.rss.FeedMessage;
 import com.frascu.bot.newsbot.rss.RSSFeedParser;
 import com.frascu.bot.newsbot.telegram.BotConfig;
+import com.frascu.bot.newsbot.telegram.handler.CommandsHandler;
 import com.frascu.bot.newsbot.telegram.handler.NewsHandler;
 
-public class NewsJob implements Job {
+// Create a class extends with TimerTask
+public class ScheduledTask extends TimerTask {
 
+	private static final Logger LOGGER = Logger.getLogger(ScheduledTask.class);
+
+	private static final int INTERVAL_SECONDS = 60;
 	private static final int MIN_OF_N_WORDS_IN_TITLE = 5;
-
 	private static final int LENGHT_OF_WORD_TO_CHECK = 5;
-
-	private static final Logger LOGGER = Logger.getLogger(NewsJob.class);
-
-	private NewsDao newsDao = NewsDao.getInstance();
 
 	private static boolean isFirstRun = true;
 
-	public NewsJob() {
-		super();
-	}
-
-	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
+	// Add your task here
+	public void run() {
 		try {
-
 			// Read RSS
 			RSSFeedParser parser = new RSSFeedParser(BotConfig.getSources());
 
 			LOGGER.info("Reading the rss...");
 			List<FeedMessage> feed = parser.readFeed();
 
+			NewsDao newsDao = new NewsDao();
 			List<NewsDto> newsDtos = newsDao.saveNewsFromFeeds(feed);
 
 			List<NewsDto> newsToSend = new ArrayList<>();
@@ -70,9 +68,9 @@ public class NewsJob implements Job {
 
 				// Send the similar news to the admin
 				newsHandler.sendNewsToAdmin(newsSimilar);
-				
+
 			} else {
-				setFirstRun(false);
+				ScheduledTask.isFirstRun = false;
 			}
 
 		} catch (Exception e) {
@@ -80,8 +78,22 @@ public class NewsJob implements Job {
 		}
 	}
 
-	public static void setFirstRun(boolean isFirstRun) {
-		NewsJob.isFirstRun = isFirstRun;
+	public void init() {
+		// The job that answer to the user
+		try {
+			ApiContextInitializer.init();
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+			telegramBotsApi.registerBot(new CommandsHandler());
+		} catch (TelegramApiException e) {
+			LOGGER.error("Impossible to create the bot.", e);
+		}
 	}
 
+	public static void main(String[] args) {
+		ScheduledTask st = new ScheduledTask();
+		st.init();
+
+		Timer time = new Timer();
+		time.schedule(st, 0, INTERVAL_SECONDS * 1000);
+	}
 }

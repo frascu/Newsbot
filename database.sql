a6dd07478e252eadc1d7933412ff34adc9deffbc
frascu <frascu@users.noreply.github.com>
2017-05-01 23:45:22 +0200

    Fix SQL script

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE `newsbot` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-CREATE TABLE `group_telegram` (
+CREATE TABLE GROUP_TELEGRAM (
   `group_id` bigint(20) NOT NULL,
   `registered` varchar(1) DEFAULT NULL,
   PRIMARY KEY (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `news` (
+CREATE TABLE NEWS (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `link` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE `news` (
   UNIQUE KEY `link` (`link`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `user_telegram` (
+CREATE TABLE USER_TELEGRAM (
   `user_id` bigint(20) NOT NULL,
   `user_name` varchar(150) DEFAULT NULL,
   `first_name` varchar(150) DEFAULT NULL,
